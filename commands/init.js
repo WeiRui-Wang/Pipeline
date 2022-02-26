a29d9f6d80500a9c12b2d682643a2c48dd193992
kpeng <kpeng@ncsu.edu>
2022-02-26 17:07:05 -0500

    first commit

--- commands/init.js
+++ commands/init.js
@@ -1,5 +1,13 @@
 const chalk = require('chalk');
 const path = require('path');
+const exec = require('child_process').exec;
+
+const VBoxManage = require('../lib/exec/VBoxManage');
+const sshExec = require('../lib/exec/ssh');
+const { stderr, stdout, stdin } = require('process');
+
+let image_dir = path.basename('ubuntu-focal-20.04')
+let image = path.join(image_dir, 'box.ovf');
 
 exports.command = 'init';
 exports.desc = 'Prepare tool';
@@ -11,7 +19,11 @@ exports.builder = yargs => {
 
 exports.handler = async argv => {
     const { processor } = argv;
+    exec('bakerx pull focal cloud-images.ubuntu.com',(error, stdout, stderr) => {
 
-    console.log(chalk.green("Preparing computing environment..."));
-   
+        console.log(error || stderr);
+        console.log(stdout);
+    });
+    // await VBoxManage.execute("import", `"${image}" --vsys 0 --vmname config-server`);
+
 };
